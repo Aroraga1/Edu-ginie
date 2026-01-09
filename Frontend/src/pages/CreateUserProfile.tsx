b726e467e02b043acc9b3e0c6dd587e5dd7e2cd7
Garvit Arora <aroragarvit1805@gmail.com>
2026-01-09 17:22:59 +0530

    exgtra

--- Frontend/src/pages/CreateUserProfile.tsx
+++ Frontend/src/pages/CreateUserProfile.tsx
@@ -20,7 +20,7 @@ const CreateUserProfile = () => {
   const [existingUser, setExistingUser] = useState<any>(null);
 
   // Form state
-  //
+  ///
   const [formData, setFormData] = useState({
     bio: "",
     interests: "",
